3e8289b0fda1c7276eaae5ccd14e683da727c10c
anilkumarhiremath <anilkumarswamy9@gmail.com>
2025-01-10 22:24:04 +0530

    Architect Assain development

--- src/Demo/com/EmailValidator.java
+++ src/Demo/com/EmailValidator.java
@@ -4,10 +4,15 @@ import java.util.regex.Pattern;
 
 public class EmailValidator {
     public static void main(String[] args) {
+    	
+    	// Here regex having the valid email pattern
         String regex = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
         String email = "example@example.com";
         
+        //Here compiles the pattern and stores in the pattern variable
         Pattern pattern = Pattern.compile(regex);
+        
+        //Here matcher class will help to verify the valid pattern and returns boolean value
         Matcher matcher = pattern.matcher(email);
         
         if (matcher.matches()) {
